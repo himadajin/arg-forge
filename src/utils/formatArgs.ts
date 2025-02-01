bb73d719cb1d55bd6a54822e7f52613d5462f5f5
Taiki Harada <haradataiki19990927@gmail.com>
2025-02-01 22:26:42 +0900

    fix: update formatArgs to handle different argument types correctly

--- src/utils/formatArgs.ts
+++ src/utils/formatArgs.ts
@@ -1,9 +1,11 @@
 export const formatArgs = (args: any[]): string[] => {
   return args.reduce((acc: string[], arg) => {
-    if (arg.option) {
+    if (arg.type === "option-equal") {
+      acc.push(`${arg.option}=${arg.value}`);
+    } else if (arg.type === "option" || arg.type === "option-space") {
       acc.push(arg.option);
-      if (arg.value) acc.push(arg.value);
-    } else if (arg.value) {
+      if (arg.value && arg.type === "option-space") acc.push(arg.value);
+    } else if (arg.type === "value") {
       acc.push(arg.value);
     }
     return acc;
