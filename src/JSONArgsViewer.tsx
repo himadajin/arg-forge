e607b63a6003f8a6c38d7abfc117060eceab519c
Taiki Harada <haradataiki19990927@gmail.com>
2025-02-02 15:07:01 +0900

    feat: add copy button to JSONArgsViewer for easy JSON string copying

--- src/JSONArgsViewer.tsx
+++ src/JSONArgsViewer.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, Button } from '@chakra-ui/react';
+import { useClipboard } from '@chakra-ui/hooks';
+import { CopyIcon } from '@chakra-ui/icons';
 
 import { formatArgs } from './utils/formatArgs';
 import { Argument } from './parser';
@@ -11,17 +13,30 @@ interface JSONArgsViewerProps {
 const JSONArgsViewer: React.FC<JSONArgsViewerProps> = ({ parsedArgs }) => {
   const formatted = React.useMemo(() => formatArgs(parsedArgs), [parsedArgs]);
   const jsonString = JSON.stringify({ args: formatted }, null, 2);
+  const { onCopy } = useClipboard(jsonString);
 
   return (
-    <Box
-      as="pre"
-      p={4}
-      bg="gray.50"
-      borderWidth="1px"
-      borderRadius="md"
-      overflow="auto"
-    >
-      {jsonString}
+    <Box position="relative">
+      <Button
+        onClick={onCopy}
+        size="sm"
+        position="absolute"
+        right="4"
+        top="4"
+        variant="ghost"
+      >
+        <CopyIcon />
+      </Button>
+      <Box
+        as="pre"
+        p={4}
+        bg="gray.50"
+        borderWidth="1px"
+        borderRadius="md"
+        overflow="auto"
+      >
+        {jsonString}
+      </Box>
     </Box>
   );
 };
